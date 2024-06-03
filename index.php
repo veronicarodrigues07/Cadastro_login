d02b52e642da2e3d1f4277b127c6fa6c3598e100
veronicarodrigues07 <127853034+veronicarodrigues07@users.noreply.github.com>
2024-06-03 09:23:20 -0300

    Modificando informações na pagina de inicio

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
 
   <main class="px-3">
     <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">Essa página é a página principal de um pequeno projeto criado para o ensino de acesso à base de dados utilizando PHP como linguagem de back-end.</p>
     <p class="lead">
       <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
     </p>
